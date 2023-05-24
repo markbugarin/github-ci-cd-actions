fc1481cfb2b806471483d0724d357ed3d10cd420
markbugarin <markbgrn@gmail.com>
2023-05-24 12:35:58 +0800

    Initial Commit

---
+++ src/main/java/com/markbgrn/cicd/GithubCiCdActionsApplication.java
@@ -0,0 +1,13 @@
+package com.markbgrn.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCiCdActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCiCdActionsApplication.class, args);
+    }
+
+}
